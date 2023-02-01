36bfed1af2f502abefd699cfd906446e48f63a11
Burhan Qadri <burhan.qadri@alumni.ubc.ca>
2023-02-01 08:14:18 -0500

    first-commit

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+  brand: {
+    primary: "#ff5a00",
+    secondary: "#ffa500",
+    muted: "#ffa500",
+  },
+  ui: {
+    primary: "#262626",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#FFFFFF",
+    secondary: "#F1F1F1",
+  },
+};
---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,13 @@
+import { fontSizes, fontWeights, fonts } from "./fonts";
+import { lineHeights, space } from "./spacing";
+
+import { colors } from "./colors";
+
+export const theme = {
+  colors,
+  space,
+  lineHeights,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
