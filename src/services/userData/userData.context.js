fa6d96f3081858004c9c942978b2e0338802d91e
Burhan Qadri <burhan.qadri@alumni.ubc.ca>
2023-02-02 11:01:37 -0500

    *****removed quiz, startqs, and interestedbenefits

--- src/services/userData/userData.context.js
+++ src/services/userData/userData.context.js
@@ -1,26 +1,24 @@
-// check if the user doesn't have an account but clicked the I have an account button (do this on the phone)
-//@collapse
-
 import * as Notifications from "expo-notifications";
+
 import {
   ADD_NEW_USER,
   GET_USER_BY,
   UPDATE_USER,
 } from "./queries/users.queries";
 import { AppState, StyleSheet, Text, View } from "react-native";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import { req_addUser, req_getThisUser } from "./requests/users.requests";
 import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
-import gql from "graphql-tag";
 
 // ****************************************
 export const UserDataContext = createContext();
 
 export const UserDataContextProvider = ({ children }) => {
-  const [get_thisUser] = useLazyQuery(GET_USER_BY, {
-    fetchPolicy: "network-only",
-  });
-  const [do_addUser, {}] = useMutation(ADD_NEW_USER);
-  const [do_updateUser, {}] = useMutation(UPDATE_USER);
+  // const [get_thisUser] = useLazyQuery(GET_USER_BY, {
+  //   fetchPolicy: "network-only",
+  // });
+  // const [do_addUser, {}] = useMutation(ADD_NEW_USER);
+  // const [do_updateUser, {}] = useMutation(UPDATE_USER);
 
   // ************************************************************************************************
   const appState = useRef(AppState.currentState);
@@ -28,7 +26,7 @@ export const UserDataContextProvider = ({ children }) => {
 
   // Useffect for when to update  *******************************************************************
   useEffect(() => {
-    console.log("STARTING);
+    console.log("STARTING");
     const subscription = AppState.addEventListener(
       "change",
       async (nextAppState) => {
@@ -53,7 +51,8 @@ export const UserDataContextProvider = ({ children }) => {
   // users
   //
   async function func_addUser() {
-    await req_addUser({ uid }, do_addHabit, do_addUser, curTime, curUser);
+    return;
+    // await req_addUser({ uid }, do_addHabit, do_addUser, curTime, curUser);
 
     // await Notifications.cancelAllScheduledNotificationsAsync();
 
@@ -75,17 +74,17 @@ export const UserDataContextProvider = ({ children }) => {
   }
 
   async function func_getThisUser(save = true) {
-    const data = await req_getThisUser({ uid }, get_thisUser);
-    return data;
+    return;
+    // const data = await req_getThisUser({ uid }, get_thisUser);
+    // return data;
   }
-
 
   return (
     <UserDataContext.Provider
       value={{
         func_addUser,
         func_getThisUser,
-        func_updateUser,
+        // func_updateUser,
       }}
     >
       {children}
